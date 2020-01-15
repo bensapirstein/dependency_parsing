5438f3e434d4f30855c31422a7d56c9321b89a51
ben_sapir <ben_sapir@cs.huji.ac.il>
2020-01-15 17:28:38 +0200

    changed to sparse matrix

--- ex4.py
+++ ex4.py
@@ -3,6 +3,7 @@ from nltk.corpus import dependency_treebank
 import numpy as np
 import Chu_Liu_Edmonds_algorithm as cle
 from collections import defaultdict
+from scipy.sparse import csr_matrix
 
 nltk.download("dependency_treebank")
 
@@ -19,31 +20,33 @@ class MST:
     def update_weights(self, w, mst, s, lr=1):
         for arc in mst.values():
             arc_features = self.feature(arc.head, arc.tail, s)
-            for k in arc_features:
-                w[k] -= lr * arc_features[k]
+            w -= arc_features * lr
+            # for k in arc_features:
+            #     w[k] -= lr * arc_features[k]
         for node in s.nodes.values():
             if node["head"] is not None:
                 arc_features = self.feature(node["head"], node["address"], s)
-                for k in arc_features:
-                    w[k] += lr * arc_features[k]
+                w += lr * arc_features
+                # for k in arc_features:
+                #     w[k] += lr * arc_features[k]
         return w
 
     def perceptron(self, num_iterations, lr):
-        Ws = [defaultdict(float)]
+        Ws = [csr_matrix((words_count ** 2 + tags_count ** 2, 1))]
         N = num_iterations * len(self.train)
-        w_final = defaultdict(float)
+
         for i in range(num_iterations):
             for j, parsed_sent in enumerate(self.train):
                 arcs = self.generate_arcs_from_sent(parsed_sent, Ws[-1])
                 mst = cle.min_spanning_arborescence(arcs, 0)
                 new_w = self.update_weights(Ws[-1], mst, parsed_sent, lr)
                 Ws.append(new_w)
-                if j % 100 is 0:
-                    print("Parsed %d/%d.." % (j + 1, N))
-                for k in new_w:
-                    w_final[k] += w_final[k] + float(new_w[k]) / N
+                # if j % 100 is 0:
+                print("Parsed %d/%d.." % (j + 1, N))
 
-
+        w_final = csr_matrix((words_count ** 2 + tags_count ** 2, 1))
+        for w in Ws:
+            w_final += w / N
         print("Finished calculating the perceptron")
         return w_final
 
@@ -60,7 +63,8 @@ class MST:
 
     def score(self, u, v, s, w):
         f = self.feature(u, v, s)
-        return -sum(w[k] * f[k] for k in f)
+        # return -sum(w[k] * f[k] for k in f)
+        return -w.T.dot(f)
 
     def eval(self, w):
         accs = []
@@ -75,7 +79,8 @@ class MST:
                 print("Evaluated %d/%d.." % (i + 1, len(self.test)))
         return np.average(accs)
 
-def default_feature(u_idx, v_idx, s):
+
+def default_feature_old(u_idx, v_idx, s):
     feature_vector = defaultdict(float)
     u_word, v_word = s.nodes[u_idx]["word"], s.nodes[v_idx]["word"]
     u_tag, v_tag = s.nodes[u_idx]["tag"], s.nodes[v_idx]["tag"]
@@ -87,18 +92,46 @@ def default_feature(u_idx, v_idx, s):
     feature_vector[(u_tag, v_tag)] = 1
     return feature_vector
 
+
+words = set(word for word, tag in dependency_treebank.tagged_words())
+words.add("ROOT")
+words = {word: i for i, word in enumerate(words)}
+tags = set(tag for word, tag in dependency_treebank.tagged_words())
+tags.add("ROOT")
+tags = {tag: i for i, tag in enumerate(tags)}
+words_count, tags_count = len(words), len(tags)
+
+
+def default_feature(u_idx, v_idx, s):
+    u_word, v_word = s.nodes[u_idx]["word"], s.nodes[v_idx]["word"]
+    u_tag, v_tag = s.nodes[u_idx]["tag"], s.nodes[v_idx]["tag"]
+    if u_idx is 0:
+        u_word, u_tag = "ROOT", "ROOT"
+    if v_idx is 0:
+        v_word, v_tag = "ROOT", "ROOT"
+
+    u_w_idx, v_w_idx = words[u_word], words[v_word]
+    u_t_idx, v_t_idx = tags[u_tag], tags[v_tag]
+    uv_w_idx = u_w_idx * words_count + v_w_idx
+    uv_t_idx = words_count ** 2 + u_t_idx * tags_count + v_t_idx
+    feature_vector = csr_matrix(((1, 1), ((uv_w_idx, uv_t_idx), (0, 0))), shape=(words_count ** 2 + tags_count ** 2, 1))
+    return feature_vector
+
+
 ONE_DIST = "one word dist"
 TWO_DIST = "two words dist"
 THREE_DIST = "three words dist"
 FOUR_PLUS_DIST = "for or more words dist"
 
-def dist_feature(u_idx,v_idx,s):
+
+def dist_feature(u_idx, v_idx, s):
     f = default_feature(u_idx, v_idx, s)
     f[ONE_DIST] = int(u_idx - v_idx == 1)
     f[TWO_DIST] = int(u_idx - v_idx == 2)
     f[THREE_DIST] = int(u_idx - v_idx == 3)
     f[FOUR_PLUS_DIST] = int(u_idx - v_idx >= 4)
     return f
+
 
 def main():
     default_mst = MST(default_feature)
@@ -111,6 +144,7 @@ def main():
     dist_acc = dist_mst.eval(dist_w)
     print(dist_acc)
     print("%.2f" % dist_acc)
+
 
 if __name__ == "__main__":
     main()
